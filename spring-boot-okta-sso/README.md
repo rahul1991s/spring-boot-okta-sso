a176b98d1c639429d6b2272471ccdcc570476909
Rahul S <rahul191291@gmail.com>
2021-04-14 18:39:38 +0800

    Initial commit auth part

---
+++ spring-boot-okta-sso/README.md
@@ -0,0 +1,4 @@
+# spring-boot-okta-sso
+How to Add Single Sign-On to Your Spring Boot Web Application using okta and OAuth 2.0
+
+#Can use Saml token to authorize the user and to fetch user profiles 
---
+++ spring-boot-okta-sso/src/main/java/com/rahuls/spring/okta/sso/SpringBootOktaSsoApplication.java
@@ -0,0 +1,25 @@
+package com.rahuls.spring.okta.sso;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SpringBootOktaSsoApplication {
+
+	@GetMapping("/")
+	public String welcome2User(Principal principal) {
+		return "Hi "+principal.getName()+" Welcome to Application";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOktaSsoApplication.class, args);
+	}
+
+}
